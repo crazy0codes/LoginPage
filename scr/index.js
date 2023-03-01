631a15ec8b9addb9ffb95cabc4b086813412a51d
crazy0codes <madhanpanja@gmail.com>
2023-03-02 00:04:54 +0530

    completed

--- scr/index.js
+++ scr/index.js
@@ -1,31 +1,50 @@
-const emailInput = document.querySelector('input[type="email"]');
-const passwordInput = document.querySelector('input[type="password"]');
+const newUserEmail = document.querySelectorAll('input[type="email"]')[0];
+const newUserPassword = document.querySelectorAll('input[type="password"]')[0];
+
+const oldUserEmail = document.querySelectorAll('input[type="email"]')[1];
+const oldUserPassword = document.querySelectorAll('input[type="password"]')[1];
+
 const submit = document.querySelector('button[type="submit"]');
-const genderInput = document.querySelectorAll('input[type="radio"]');
+const genderInput = document.querySelector('input[type="radio"]');
 const newUser = document.querySelector('.register')
 const oldUser = document.querySelector('.loginNow')
 
-let Link = document.querySelectorAll('a')[0];
-let Links = document.querySelectorAll('a')[1];
+let Link = document.querySelector('#login');
+let Links = document.querySelector('#register');
+
+
 
 //Errors
-const emailError = document.querySelector('#emailError');
-const passwordError = document.querySelector("#passwordError")
+const registerEmailError = document.querySelectorAll('.emailError')[0];
+const registerPasswordError = document.querySelectorAll(".passwordError")[0];
 
-emailInput.addEventListener('input',function(){
-  if(/[\s]/g.test(this.value)) {
-   emailError.innerHTML = "&#10071;Enter a valid email";
-   this.style.outlineColor = 'red';
-   }
-   else{
-    emailError.innerText = "";
-    this.style.outlineColor = 'rgb(21, 209, 18)';
-   }
-})
+const loginEmailError = document.querySelectorAll('.emailError')[1];
+const loginPasswordError = document.querySelectorAll(".passwordError")[1];
 
-passwordInput.addEventListener('input',function(){
-    if(this.value == ""){passwordError.innerHTML = "&#10071;Enter a valid password"} else {passwordError.innerText = ''}
-})
+//Checking
+newUserEmail.addEventListener('input',() => checkEmail(registerEmailError));
+oldUserEmail.addEventListener('input',() => checkEmail(loginEmailError));
+
+function checkEmail(Error){
+    if(/[\s]/g.test(Error.previousElementSibling.value)) {
+     Error.innerHTML = "&#10071;Enter a valid email";
+     Error.previousElementSibling.style.outlineColor = 'red';
+     }
+     else{
+      Error.innerText = "";
+      Error.previousElementSibling.style.outlineColor = 'rgb(21, 209, 18)';
+     }
+}
+
+newUserPassword.addEventListener('input',() => checkPassword(registerPasswordError));
+oldUserPassword.addEventListener('input',() => checkPassword(loginPasswordError));
+
+function checkPassword(Error){
+    if(Error.previousElementSibling.value == ""){
+        Error.innerHTML = "&#10071;Enter a valid password"}
+    else {
+        Error.innerHTML = ''}
+}
 
 Links.onclick = link
 Link.onclick = link
